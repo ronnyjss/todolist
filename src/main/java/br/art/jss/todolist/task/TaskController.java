9aca40fb86d43cb7cf973b4e9d9c3a0101d34625
Ronny Silva <ronny@jss.art.br>
2023-10-13 20:56:04 -0300

    [A4] feat: Adicionando listagem de tasks por usuario autenticado

--- src/main/java/br/art/jss/todolist/task/TaskController.java
+++ src/main/java/br/art/jss/todolist/task/TaskController.java
@@ -1,13 +1,15 @@
 package br.art.jss.todolist.task;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.List;
+import java.time.LocalDateTime;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,4 +49,11 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        UUID idUser = (UUID) request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser(idUser);
+
+        return tasks;
+    }
 }
