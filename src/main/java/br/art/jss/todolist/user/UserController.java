d61132f7f9d0e32e25730e55fcdac664859cc764
Ronny Silva <ronny@jss.art.br>
2023-10-13 13:31:13 -0300

    [A3] feat: add lib bcrypt, criptografando senha

--- src/main/java/br/art/jss/todolist/user/UserController.java
+++ src/main/java/br/art/jss/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.art.jss.todolist.user;
 
+import org.aspectj.weaver.bcel.BcelAccessForInlineMunger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -26,6 +29,12 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt
+            .withDefaults()
+            .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
 
