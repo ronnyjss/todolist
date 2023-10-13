793e75ddeea134c9be45b683ffb19a1360b66a82
Ronny Silva <ronny@jss.art.br>
2023-10-13 13:21:49 -0300

    [A2] feat: Utilizando response.

--- src/main/java/br/art/jss/todolist/user/UserController.java
+++ src/main/java/br/art/jss/todolist/user/UserController.java
@@ -1,7 +1,8 @@
 package br.art.jss.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StopWatch;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,17 +20,16 @@ public class UserController {
      * @param userModel
      */
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            System.out.println("Usuário já existe");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
 
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
